d36f1eacf1d704f48f3a3b78e4b73467680e56fb
Susan Brown <brown684408@gmail.com>
2022-09-24 17:57:09 -0500

    associate pizza and comment models

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,23 +1,44 @@
 const { Schema, model } = require('mongoose');  // this imports the Schema constructor, and model function from the mongoose library
 
 // this creates the schema for the model
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String
-  },
-  createdBy: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  size: {
-    type: String,
-    default: 'Large'
-  },
-  toppings: []
-});
+const PizzaSchema = new Schema(
+    {
+      pizzaName: {
+        type: String
+      },
+      createdBy: {
+        type: String
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      size: {
+        type: String,
+        default: 'Large'
+      },
+      toppings: [],
+      // the "parent" Pizza, is associated with "child" comment here. ref tells it to search the Comment document
+      comments: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Comment'
+        }
+      ]
+    },
+    {
+        // this "toJSON" tells the schema that it can use virtuals 
+      toJSON: {
+        virtuals: true,
+      },
+      id: false
+    }
+  );
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+  });
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
